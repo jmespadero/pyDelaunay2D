ce71d4a18e2c07f60ae1e7bf3fa80ab888026309
jmespadero <josemiguel.espadero@urjc.es>
2016-12-14 10:48:39 +0100

    PEP8 code style

--- delaunay2D.py
+++ delaunay2D.py
@@ -1,14 +1,15 @@
 """
 Simple structured Delaunay triangulation in 2D with Bowyer–Watson algorithm.
-  
+
 Written by Jose M. Espadero ( http://github.com/jmespadero/pyDelaunay2D )
 Based on code from Ayron Catteau. Published at http://github.com/ayron/delaunay
 
 Just pretend to be simple and didactic. The only requisite is numpy.
-Lack of robustness checks, so do not expect to work on degenerate set of points.
+Lack of robustness checks. Do not expect to work on degenerate set of points.
 """
 
 import numpy as np
+
 
 class Triangle:
     """Simple class to store an indexed triangle with neighbour information."""
@@ -16,7 +17,7 @@ class Triangle:
     def __init__(self, a, b, c):
         """Constructor from 3 indexes to vertex"""
         self.v = [a, b, c]
-        self.neighbour = [None]*3  # Adjacent triangles
+        self.neighbour = [None] * 3  # Adjacent triangles
         # Avoid computing circumcenter here, so the triangle does
         # not need to know its coordinates
         self.center = None
@@ -25,7 +26,8 @@ class Triangle:
     def __repr__(self):
         """Dump indexes as a text string"""
         return 'tri< ' + str(self.v) + ' >'
-        
+
+
 class Delaunay2D:
     """
     Class to compute a Delaunay triangulation in 2D
@@ -35,24 +37,24 @@ class Delaunay2D:
 
     def __init__(self, seeds, margin=None):
         """Create a new set of triangles from a 2D vertex array
-        seeds  -- Array of 2D coordinates for the vertex 
+        seeds  -- Array of 2D coordinates for the vertex
         margin -- Optional parameter to set the distance of extended BBox
         """
-        
+
         seeds = np.asarray(seeds)
-        
+
         # Compute BBox
-        smin= np.amin(seeds, axis=0)
-        smax= np.amax(seeds, axis=0)
+        smin = np.amin(seeds, axis=0)
+        smax = np.amax(seeds, axis=0)
         # If no margin is given, compute one based on the diagonal
         if not margin:
-            margin = 50*np.linalg.norm(smax- smin)
-            
+            margin = 50 * np.linalg.norm(smax - smin)
+
         # Extend the BBox coordinates by the margin. Store it.
         smin -= margin
         smax += margin
         self.margin = margin
-        
+
         # Create a two triangles 'frame' big enough to contains all seeds
         self.coords = [smin, np.array((smax[0], smin[1])),
                        smax, np.array((smin[0], smax[1]))]
@@ -63,50 +65,50 @@ class Delaunay2D:
         T2.neighbour[0] = T1
         T1.center, T1.radius = self.Circumcenter(T1)
         T2.center, T2.radius = self.Circumcenter(T2)
-        self.triangles = [T1, T2]        
+        self.triangles = [T1, T2]
 
         # Insert all seeds one by one
         for s in seeds:
             self.AddPoint(s)
-            
+
     def Circumcenter(self, tri):
         """Compute Circumcenter and circumradius of a triangle.
         Uses an extension of the method described here:
         http://www.ics.uci.edu/~eppstein/junkyard/circumcenter.html
         """
-        pts = np.asarray([self.coords[v] for v in tri.v])              
-        A = np.bmat( [[ 2*np.dot(pts,pts.T), np.ones((3,1)) ],
-                   [  np.ones((1,3)) ,  np.zeros((1,1))  ]] )
+        pts = np.asarray([self.coords[v] for v in tri.v])
+        A = np.bmat([[2 * np.dot(pts, pts.T), np.ones((3, 1))],
+                     [np.ones((1, 3)),  np.zeros((1, 1))]])
 
         b = np.hstack((np.sum(pts * pts, axis=1), np.ones((1))))
-        x = np.linalg.solve(A,b)
-        bary_coords = x[:-1]  
-        
-        center = np.dot(bary_coords,pts)    
-        #radius = np.linalg.norm(pts[0] - center) # euclidean distance
-        radius = np.sum(np.square(pts[0] - center)) # squared distance
-        #print("center:", center, "radius:", radius)
-        return (center,radius)    
+        x = np.linalg.solve(A, b)
+        bary_coords = x[:-1]
 
-    def AddPoint(self, p):       
+        center = np.dot(bary_coords, pts)
+        # radius = np.linalg.norm(pts[0] - center) # euclidean distance
+        radius = np.sum(np.square(pts[0] - center))  # squared distance
+        # print("center:", center, "radius:", radius)
+        return (center, radius)
+
+    def AddPoint(self, p):
         """Add a new point to the current triangulation.
         """
-        p = np.asarray(p)        
+        p = np.asarray(p)
         idx = len(self.coords)
-        #print("coords[", idx,"] ->",p)
+        # print("coords[", idx,"] ->",p)
         self.coords.append(p)
-        
+
         # Search the triangle(s) whose circumcircle contains p
         bad_triangles = []
         for T in self.triangles:
-            #if np.linalg.norm(T.center - p) <= T.radius: # euclidean distance
-            if np.sum(np.square(T.center - p)) <= T.radius: # squared distance
+            # if np.linalg.norm(T.center - p) <= T.radius: # euclidean distance
+            if np.sum(np.square(T.center - p)) <= T.radius:  # squared distance
                 bad_triangles.append(T)
 
         # Remove triangles too near of point p
         for T in bad_triangles:
             self.triangles.remove(T)
-            
+
         # Find the CCW boundary (star shape) of the bad triangles,
         # expressed as a list of edges (point pairs) and the opposite
         # triangle to each edge.
@@ -116,22 +118,23 @@ class Delaunay2D:
         edge = 0
         while True:
             # Check if edge is on the boundary...
-            # if opposite triangle of this edge is external to the list            
+            # if opposite triangle of this edge is external to the list
             if not T.neighbour[edge] in bad_triangles:
                 # Insert edge and external triangle into boundary list
-                boundary.append((T.v[(edge+1)%3], T.v[(edge-1)%3], T.neighbour[edge]))
+                boundary.append(
+                    (T.v[(edge+1) % 3], T.v[(edge-1) % 3], T.neighbour[edge]))
 
                 # Move to next CCW edge in this triangle
                 edge = (edge + 1) % 3
-                
+
                 # Check if boundary is a closed loop
                 if boundary[0][0] == boundary[-1][1]:
                     break
-            else:    
+            else:
                 # Move to next CCW edge in neighbour triangle
                 last = T
                 T = T.neighbour[edge]
-                edge = (T.neighbour.index(last) + 1) % 3 
+                edge = (T.neighbour.index(last) + 1) % 3
 
         # Retriangle the hole left by bad_triangles
         new_triangles = []
@@ -142,14 +145,14 @@ class Delaunay2D:
 
             # Set external triangle stored at edge[2] as neighbour of T
             T.neighbour[0] = edge[2]
-            
+
             # Set T as neighbour of triangle stored at edge[2]
             if T.neighbour[0]:
-                #search the reversed edge in T.neighbour[0].v
+                # search the reversed edge in T.neighbour[0].v
                 tmp_v = T.neighbour[0].v
                 for i in range(3):
-                    if edge[1] == tmp_v[i] and edge[0] == tmp_v[(i+1)%3]:
-                        T.neighbour[0].neighbour[(i-1)%3] = T
+                    if edge[1] == tmp_v[i] and edge[0] == tmp_v[(i + 1) % 3]:
+                        T.neighbour[0].neighbour[(i - 1) % 3] = T
 
             # Add triangle to a temporal list
             new_triangles.append(T)
@@ -157,29 +160,29 @@ class Delaunay2D:
         # Link the new triangles
         N = len(new_triangles)
         for i, T in enumerate(new_triangles):
-            T.neighbour[2] = new_triangles[(i-1) % N]   # back
-            T.neighbour[1] = new_triangles[(i+1) % N]   # forward
-   
+            T.neighbour[2] = new_triangles[(i - 1) % N]   # back
+            T.neighbour[1] = new_triangles[(i + 1) % N]   # forward
+
         # Add triangles to our triangulation
         self.triangles.extend(new_triangles)
-      
+
     def exportDT(self):
         """Export the current Delaunay Triangulation.
         """
         # Filter out coordinates in the extended BBox
-        xs = [p[0] for p in self.coords[4:] ]
-        ys = [p[1] for p in self.coords[4:] ]        
-        
+        xs = [p[0] for p in self.coords[4:]]
+        ys = [p[1] for p in self.coords[4:]]
+
         # Filter out triangles with any vertex in the extended BBox
-        ETS = [t.v for t in self.triangles ]
-        tris = [(a-4,b-4,c-4) for (a,b,c) in ETS if a > 3 and b > 3 and c > 3]
+        ETS = [t.v for t in self.triangles]
+        tris = [(a - 4, b - 4, c - 4)
+                for (a, b, c) in ETS if a > 3 and b > 3 and c > 3]
         return xs, ys, tris
-        
+
     def exportExtendedDT(self):
         """Export the Extended Delaunay Triangulation (include the frame vertex).
         """
-        xs = [p[0] for p in self.coords ]
-        ys = [p[1] for p in self.coords ]        
-        tris = [t.v for t in self.triangles ]
+        xs = [p[0] for p in self.coords]
+        ys = [p[1] for p in self.coords]
+        tris = [t.v for t in self.triangles]
         return xs, ys, tris
-
--- delaunay2D_test.py
+++ delaunay2D_test.py
@@ -2,21 +2,22 @@
 # -*- coding: utf-8 -*-
 """
 Simple delaunay2D test
-  
+
 Written by Jose M. Espadero <josemiguel.espadero@urjc.es>
 """
 import numpy as np
 import delaunay2D as D
 
 if __name__ == '__main__':
-    
-    ###########################################################        
+
+    ###########################################################
     # Generate 'numSeeds' random seeds in a square of size 'radius'
     numSeeds = 24
     radius = 100
     seeds = radius * np.random.random((numSeeds, 2))
     print("seeds:\n", seeds)
-    print("BBox Min:", np.amin(seeds, axis=0), "Bbox Max: ", np.amax(seeds, axis=0) )
+    print("BBox Min:", np.amin(seeds, axis=0),
+          "Bbox Max: ", np.amax(seeds, axis=0))
 
     # Compute our Delaunay triangulation of seeds.
     # It is recommended to use the radius to create a noticeable margin
@@ -41,15 +42,14 @@ if __name__ == '__main__':
     # Plot our Delaunay triangulation (plot in blue)
     ax.triplot(matplotlib.tri.Triangulation(DT_x, DT_y, DT_tris), 'bo-.')
 
-    #DEBUG: Use matplotlib method to create a Delaunay triangulation (plot in green)
-    #DEBUG: It should be equal to our result in DT_tris (plot in blue)
-    #DEBUG: If boundary is diferent, try to increase the value of your margin
-    #ax.triplot(matplotlib.tri.Triangulation(DT_x, DT_y), 'g-.')
+    # DEBUG: Use matplotlib to create a Delaunay triangulation (plot in green)
+    # DEBUG: It should be equal to our result in DT_tris (plot in blue)
+    # DEBUG: If boundary is diferent, try to increase the value of your margin
+    # ax.triplot(matplotlib.tri.Triangulation(DT_x, DT_y), 'g-.')
 
-    #DEBUG: plot our extended triangulation (plot in red)
-    #EDT_x, EDT_y, EDT_tris = DT.exportExtendedDT()
-    #print("Extended DT_tris:", len(EDT_tris), "triangles")
-    #ax.triplot(matplotlib.tri.Triangulation(EDT_x, EDT_y, EDT_tris), 'ro-.')
+    # DEBUG: plot our extended triangulation (plot in red)
+    # EDT_x, EDT_y, EDT_tris = DT.exportExtendedDT()
+    # print("Extended DT_tris:", len(EDT_tris), "triangles")
+    # ax.triplot(matplotlib.tri.Triangulation(EDT_x, EDT_y, EDT_tris), 'ro-.')
 
     plt.show()
-
